c1e2d39c5c289afa75685b3e1e4587a425ebbe18
Maxim Baraliuc <m.baraliuc@gmail.com>
2023-11-27 14:34:59 +0100

    Init commit

---
+++ src/main/java/be/intecbrusse/projectmanagerbackend/ProjectManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package be.intecbrusse.projectmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/intecbrusse/projectmanagerbackend/ProjectManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.intecbrusse.projectmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
