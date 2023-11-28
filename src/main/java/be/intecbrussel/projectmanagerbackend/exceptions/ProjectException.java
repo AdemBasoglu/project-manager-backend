58d3fd5445d4829f2ecbd370f166c6fa9ff45139
Adem Basoglu <admbasoglu28@gmail.com>
2023-11-28 14:24:58 +0100

    {upd} exceptions veranderen

--- src/main/java/be/intecbrussel/projectmanagerbackend/exceptions/ProjectException.java
+++
@@ -1,7 +0,0 @@
-package be.intecbrussel.projectmanagerbackend.exceptions;
-
-public class ProjectException extends RuntimeException{
-    public ProjectException(String message){super(message);}
-
-
-}
