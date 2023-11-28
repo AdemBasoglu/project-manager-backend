b1bf20cbb504a14807e1b78090af670daad63904
Maxim Baraliuc <m.baraliuc@gmail.com>
2023-11-28 11:03:26 +0100

    [upd] add GlobalExceptionHandler

--- src/main/java/be/intecbrussel/projectmanagerbackend/services/TeamService.java
+++ src/main/java/be/intecbrussel/projectmanagerbackend/services/TeamService.java
@@ -1,4 +1,4 @@
-package be.intecbrussel.projectmanagerbackend.services;
-
-public interface TeamService {
-}
+// package be.intecbrussel.projectmanagerbackend.services;
+//
+// public interface TeamService {
+// }
