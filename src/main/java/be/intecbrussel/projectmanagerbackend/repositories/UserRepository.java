e4cf4b562e3a7525168f3c676c12da4702248e98
Marwane265 <Marwane6.35@hotmail.com>
2023-11-29 14:20:52 +0100

    [upd] post man crd

--- src/main/java/be/intecbrussel/projectmanagerbackend/repositories/UserRepository.java
+++ src/main/java/be/intecbrussel/projectmanagerbackend/repositories/UserRepository.java
@@ -4,12 +4,13 @@ import be.intecbrussel.projectmanagerbackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
 
 
 
-    List<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }
