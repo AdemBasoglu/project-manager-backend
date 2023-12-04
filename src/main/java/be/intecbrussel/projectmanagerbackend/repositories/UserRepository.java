8fb003ba158afbec1e1c8c87ed048ff676724490
Maxim Baraliuc <m.baraliuc@gmail.com>
2023-12-04 15:36:46 +0100

    [upd] add get userEmail by projectId

--- src/main/java/be/intecbrussel/projectmanagerbackend/repositories/UserRepository.java
+++ src/main/java/be/intecbrussel/projectmanagerbackend/repositories/UserRepository.java
@@ -2,10 +2,17 @@ package be.intecbrussel.projectmanagerbackend.repositories;
 
 import be.intecbrussel.projectmanagerbackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByEmail(String email);
+
+    @Query(value = "select users_email from user_projects up where up.projects_id = :ID",
+            nativeQuery = true)
+    List<String> findAllUserEmailsByProjectId(@Param("ID") Long projectId);
 }
--- src/main/java/be/intecbrussel/projectmanagerbackend/services/UserService.java
+++ src/main/java/be/intecbrussel/projectmanagerbackend/services/UserService.java
@@ -2,11 +2,15 @@ package be.intecbrussel.projectmanagerbackend.services;
 
 import be.intecbrussel.projectmanagerbackend.models.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User addUser(User user);
 
     User getUser(String email);
+
+    List<String> getAllUserEmailsByProject(Long projectId);
 
     User updateUser(User user, String email);
 
